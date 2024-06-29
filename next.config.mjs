68ef979d6ae1ffeb691c961be798a264e2bb120f
Alberto D.Sosa <albertodsosa@gmail.com>
2024-06-29 16:56:57 +0100

    Commit Inicial

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+import { withFaust, getWpHostname } from'@faustwp/core';
+import { createSecureHeaders } from'next-secure-headers';
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+export default withFaust({
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: ['node_modules'],
+  },
+  images: {
+    domains: [getWpHostname()],
+  },
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+  async headers() {
+    return [{ source: '/:path*', headers: createSecureHeaders({
+      xssProtection: false
+    }) }];
+  },
+});
---
+++ src/wp-templates/index.js
@@ -0,0 +1,14 @@
+import category from './category';
+import tag from './tag';
+import frontPage from './front-page';
+import page from './page';
+import single from './single';
+
+const wpTemplates = {
+  category,
+  tag,
+  'front-page': frontPage,
+  page,
+  single,
+};
+export default wpTemplates;
